7c3db5057e90135ff056fd3a0bafd50b427e5bef
Adam Dama <adamdama1@gmail.com>
2024-08-13 00:21:45 -0700

    fix reset

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,32 @@
-import { defineConfig } from 'drizzle-kit';
+import { type Config, defineConfig } from "drizzle-kit";
+
+let config: Config;
+if (process.env.NEXT_PUBLIC_ENVIRONMENT === "local") {
+  config = defineConfig({
+    schema: "./src/db/models",
+    driver: "turso",
+    dbCredentials: {
+      url: process.env.DATABASE_URL ?? "",
+    },
+    verbose: true,
+    strict: true,
+    out: "./src/db/drizzle",
+    dialect: "sqlite",
+  });
+} else {
+  config = defineConfig({
+    schema: "./src/db/models",
+    driver: "turso",
+    dbCredentials: {
+      url: process.env.DATABASE_URL ?? "",
+      authToken: process.env.DATABASE_AUTH_TOKEN,
+    },
+    verbose: true,
+    strict: true,
+    out: "./src/db/drizzle",
+    dialect: "sqlite",
+  });
+}
 
 // biome-ignore lint/style/noDefaultExport: we need this file to be a module
-export default defineConfig({
-  schema: './src/db/models',
-  driver: 'turso',
-  dbCredentials: {
-    url: process.env.DATABASE_URL ?? '',
-  },
-  verbose: true,
-  strict: true,
-  out: './src/db/drizzle',
-  dialect: 'sqlite',
-});
+export default config;
