e75d089877a6d4c1a70c0cecc73d6fb03b016c2a
Adam Dama <adamdama1@gmail.com>
2024-08-13 14:18:38 -0700

    header styling tweaks to make more responsive

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,10 +5,15 @@ import {
   MenubarMenu,
   MenubarSeparator,
   MenubarTrigger,
-} from '@/components/ui/menubar';
-import { TbChevronDown, TbCloudUpload, TbLoader2, TbRefresh } from 'react-icons/tb';
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+} from "@/components/ui/menubar";
+import {
+  TbChevronDown,
+  TbCloudUpload,
+  TbLoader2,
+  TbRefresh,
+} from "react-icons/tb";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export const Header = ({
   sessionName,
@@ -21,29 +26,35 @@ export const Header = ({
   resetSession: () => void;
   saveSession: () => void;
 }) => {
-  const lastUpdateText = lastUpdated ? lastUpdated.toLocaleString() : 'Not saved';
+  const lastUpdateText = lastUpdated
+    ? lastUpdated.toLocaleString()
+    : "Not saved";
   return (
-    <header className="bg-slate-900 flex w-full flex-wrap items-center justify-between px-3 shadow-md">
-      <div className="flex grow basis-0 justify-start">
+    <header className="bg-slate-900 flex flex-col md:flex-row w-full flex-wrap items-center justify-between px-3 shadow-md">
+      <div className="flex grow basis-0 justify-start items-center">
         <Image
-          className={'m-4 cursor-pointer object-contain max-h-[40px]'}
+          className={"m-4 cursor-pointer object-contain max-h-[40px]"}
           src="/logo.png"
           height={0}
           width={40}
-          alt={'logo'}
+          alt={"logo"}
         />
-      </div>
-      <div className="flex grow basis-0 justify-center space-x-4">
         <div className="font-bold text-3xl text-white">Site Planner</div>
       </div>
-      <div className="flex grow basis-0 justify-end space-x-4">
-        <Button variant={'secondary'} onClick={saveSession}>
+      <div className="flex grow basis-0 justify-end space-x-4 my-2">
+        <Button variant={"secondary"} onClick={saveSession}>
           <TbCloudUpload className="mr-2 text-xl" /> Save Session
         </Button>
         <Menubar>
           <MenubarMenu>
             <MenubarTrigger>
-              <div className="mr-2">{sessionName ? sessionName : <TbLoader2 className="animate-spin" />}</div>
+              <div className="mr-2">
+                {sessionName ? (
+                  sessionName
+                ) : (
+                  <TbLoader2 className="animate-spin" />
+                )}
+              </div>
               <TbChevronDown />
             </MenubarTrigger>
             <MenubarContent>
@@ -51,7 +62,9 @@ export const Header = ({
                 <TbRefresh className="mr-2 cursor-pointer" /> New Session
               </MenubarItem>
               <MenubarSeparator />
-              <div className="px-2 pt-2 text-xs font-bold uppercase text-slate-400">Last saved</div>
+              <div className="px-2 pt-2 text-xs font-bold uppercase text-slate-400">
+                Last saved
+              </div>
               <div className="px-2 pb-2 text-sm">{lastUpdateText}</div>
             </MenubarContent>
           </MenubarMenu>
